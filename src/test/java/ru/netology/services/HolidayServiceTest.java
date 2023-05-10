eff98430138b5f75b721d06d9fd58a64156014ad
Regina Faritova <faritova_regina@mail.ru>
2023-05-10 22:29:52 +0300

    add files

---
+++ src/test/java/ru/netology/services/HolidayServiceTest.java
@@ -0,0 +1,25 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HolidayServiceTest {
+
+    @Test
+    public void testCountMonths1() {
+        HolidayService service = new HolidayService();
+        int actual = service.calculate(10_000,3_000,20_000);
+        int expected = 3;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCountMonths2() {
+        HolidayService service = new HolidayService();
+        int actual = service.calculate(100_000,60_000,150_000);
+        int expected = 2;
+        Assertions.assertEquals(expected, actual);
+    }
+}
